a95ae91d1bcd161cdf4b4384c91a2c2807a1ab50
lesglaneurs <public@lesglaneurs.com>
2015-09-02 10:52:24 +0200

    Production config

--- lesglaneurs/settings/prod.py
+++ lesglaneurs/settings/prod.py
@@ -1,10 +1,6 @@
 from settings import *
 
-ALLOWED_HOSTS = ['127.0.0.1',
-                 'localhost']
+ALLOWED_HOSTS = ['lesglaneurs.com']
 
-#ALLOWED_HOSTS = ['lesglaneurs']
-
-## NE VOIT PAS LES CSS !!
 DEBUG=False
 TEMPLATE_DEBUG = False
