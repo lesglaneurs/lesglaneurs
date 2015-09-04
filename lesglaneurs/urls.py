27261c2eef5b40b9cc7dedcd0039e083d4b2051a
lesglaneurs <public@lesglaneurs.com>
2015-09-04 18:17:51 +0200

    adjust quiz link

--- lesglaneurs/urls.py
+++ lesglaneurs/urls.py
@@ -2,16 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'lesglaneurs.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-#    url(r'^$', include('test_page.urls')),
-#    url(r'^test_page/', include('test_page.urls')),
-## test_page
-#    url(r'^$', 'home'),
-#                        url(r'^quiz_typeform/', 'quiz_typeform'),
-#                        url(r'^home/', 'home'),
-
-    url(r'^admin/', include(admin.site.urls)),
+                       url(r'^$', 'lesglaneurs.views.home', name='home'),
+                       url(r'^quiz_typeform/$', 'lesglaneurs.views.quiz_typeform', name='quiz'),
+                       url(r'^admin/', include(admin.site.urls)),
 )
