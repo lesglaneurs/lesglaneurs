49b84a4e54f1927a91ddca92e3e19576bf7b71d0
lesglaneurs <public@lesglaneurs.com>
2015-09-07 12:51:13 +0200

    Manage dev and prod

--- gunicorn_start_dev.bash
+++ gunicorn_start_dev.bash
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-## sudo gunicorn lesglaneurs_01.wsgi:application --bind 127.0.0.1:8800
+## sudo gunicorn lesglaneurs.wsgi:application --bind 127.0.0.1:5555
 
 
 NAME="lesglaneurs"     
 PROJECT_NAME=lesglaneurs                       # Name of the application
 DJANGO_DIR=~/$PROJECT_NAME                          # Django project directory
-SOCKFILE=$DJANGO_DIR/run/gunicorn.sock            # we will communicate using this unix socket
+SOCKFILE=~/run/gunicorn.sock            	# to handled shared file system with VM
 USER=django                                       # the user to run as
 GROUP=webapps                                     # the group to run as
 NUM_WORKERS=3                                     # how many worker processes should Gunicorn spawn
-#DJANGO_SETTINGS_MODULE=$PROJECT_NAME/$PROJECT_NAME/settings.prod             # which settings file should Django use
+DJANGO_SETTINGS_MODULE=$PROJECT_NAME.settings.dev
 DJANGO_WSGI_MODULE=$PROJECT_NAME.wsgi                     # WSGI module name
 
 echo "Starting $NAME as `whoami`"
@@ -18,7 +18,7 @@ echo "Starting $NAME as `whoami`"
 # Activate the virtual environment
 # cd $DJANGODIR			
 # source ../bin/activate		
-# export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 # export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
 # Create the run directory if it doesn't exist
--- lesglaneurs/wsgi.py
+++ lesglaneurs/wsgi.py
@@ -7,8 +7,5 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lesglaneurs.settings.prod")
-
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
