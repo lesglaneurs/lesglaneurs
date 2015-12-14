07e7f5453d8fccb7e40a789748e43a3a5fb2d3ac
lesglaneurs <public@lesglaneurs.com>
2015-12-14 12:00:49 +0000

    Add Bocal Local Project

--- presentation/urls.py
+++ presentation/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from . import views
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
    #  url(r'^$', 'presentation.views.home', name='home'),
    #   url(r'^rebelle/$', 'presentation.views.rebelle', name='rebelle'),
-      url(r'^$', 'presentation.views.rebelle', name='rebelle'),
 
+# lesglaneurs.com/presentation/rebelle.html
+# lesglaneurs.com/presentation/bocal_local.html
+
+      url(r'^rebelle/$', 'presentation.views.rebelle', name='rebelle'),
+      url(r'^bocal_local/$', 'presentation.views.bocal_local', name='bocal_local'),
+
+      #url(r'^(?P<project_name>(.*))/$', views.project, name='project'),
 )
--- presentation/views.py
+++ presentation/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project, Story
 
 # Create your views here.
 
@@ -8,4 +9,13 @@ def home(request):
 
 def rebelle(request):
     return render(request, 'presentation/rebelle.html')
+
+def bocal_local(request):
+    return render(request, 'presentation/bocal_local.html')
+
+def project(request, project_name):
+    project = get_object_or_404(Project, name=project_name)
+    #stories = project.objects.get().all()
+    #print stories
+    return render(request, 'presentation/project.html', {'project':project})
 
