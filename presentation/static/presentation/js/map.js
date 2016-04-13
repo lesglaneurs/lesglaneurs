d64d94bf7254ba0ddb8fd7cf6a7a6e122a652ee4
Jean-Philippe NOEL <jean-philippe.noel@phynot.com>
2016-04-13 22:34:12 +0200

    Clean the JS code

--- presentation/static/presentation/js/map.js
+++ presentation/static/presentation/js/map.js
@@ -1,5 +1,7 @@
-var center = [46.3507193554773, 2.603759765625] //Montluçon
-var zoom = 6
+"use strict";
+
+var center = [46.3507193554773, 2.603759765625]; //Montluçon
+var zoom = 6;
 
 var map = L.map('mapid').setView(center, zoom);
 
@@ -12,16 +14,16 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoianBub2VsIiwiYSI6ImNpbXo5MGdnejAwbG92OWx5amt5cWV4ejAifQ.vJAEgiLq2bdVEGld5mau5A'
 }).addTo(map);
 
-var saint_pierre_d_aurillac = [44.572329, -0.19046500000001743]
-var paris = [48.856614, 2.3522219000000177]
-var guiseniers = [49.21267599999999, 1.4749530000000277]
+var saint_pierre_d_aurillac = [44.572329, -0.19046500000001743];
+var paris = [48.856614, 2.3522219000000177];
+var guiseniers = [49.21267599999999, 1.4749530000000277];
 
-var points = [saint_pierre_d_aurillac, paris, guiseniers]
+var points = [saint_pierre_d_aurillac, paris, guiseniers];
 
 for (var index in points) {
-    var point = points[index]
+    var point = points[index];
     var marker = L.marker(point).addTo(map);
-    marker.bindPopup("<b>Hello world!</b><br>I am a popup.")
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
     marker.on('mouseover', function (e) {
         this.openPopup();
     });
