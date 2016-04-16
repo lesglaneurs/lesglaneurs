9d52d02766389d44f20482cf40e5481b4d23a7a4
lesglaneurs <public@lesglaneurs.com>
2016-04-16 12:10:45 +0200

    Add Address model and EventAddress

--- presentation/admin.py
+++ presentation/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Project, Story, Event
+from .models import Project, Story, Event, Address, EventAddress
 
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(Story)
 admin.site.register(Event)
+admin.site.register(Address)
+admin.site.register(EventAddress)
