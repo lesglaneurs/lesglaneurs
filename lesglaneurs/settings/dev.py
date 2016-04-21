cc8b0518943a907f519570537b8578f6d644bd23
Jean-Philippe NOEL <jean-philippe.noel@phynot.com>
2016-04-21 15:52:04 +0200

    Display the stacktrace on the standard output

--- lesglaneurs/settings/dev.py
+++ lesglaneurs/settings/dev.py
@@ -6,3 +6,19 @@ ALLOWED_HOSTS = ['127.0.0.1',
 DEBUG=True
 TEMPLATE_DEBUG = True
 
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'DEBUG',
+        }
+    },
+}
