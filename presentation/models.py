e0f68d6cce3289e193e87223c0cbd12ffebfa6d0
Jean-Philippe NOEL <jean-philippe.noel@phynot.com>
2016-05-29 22:31:47 +0200

    Translate the table and field names

--- presentation/models.py
+++ presentation/models.py
@@ -11,29 +11,52 @@ class Project(models.Model):
     class Meta:
         verbose_name = 'Projet'
 
-
-    name = models.CharField(max_length=100, help_text="Le nom du projet")
-    owner_image = models.ImageField(upload_to=user_directory_path,
-                                    blank=True, null=True, help_text="Photo du chef du projet")
-    creation_date = models.PositiveIntegerField(blank=True, null=True, help_text="date de création")
-    contact_name = models.CharField(max_length=100,
-                                    blank=True, null=True, help_text="nom du contact de référence")
-    email = models.EmailField(blank=True, null=True, help_text="email de contact")
-    # telephone = models.DecimalField(max_digits=10, decimal_places=0, default='0600000000')
-    telephone = models.CharField(max_length=14, blank=True, null=True, help_text="numéro de téléphone")
-    web_site = models.URLField(blank=True, null=True, help_text="site Web du projet")
-
-    structure = models.CharField(max_length=100,
-                                 blank=True, null=True, help_text="structure administrative du projet")
-    location_today = models.ImageField(upload_to=user_directory_path,
-                                       blank=True, null=True, help_text="présence du projet aujourd'hui")
-    location_target = models.ImageField(upload_to=user_directory_path,
-                                        blank=True, null=True, help_text="prévision d'évolution géographique")
-    logo = models.ImageField(upload_to=user_directory_path,
-                             blank=True, null=True, help_text="photo du logo du projet")
-    project_structure = models.ImageField(upload_to=user_directory_path,
-                                          blank=True, null=True, help_text="image de la structure globale du projet")
-    workers = models.TextField(blank=True, null=True, help_text="participants au projet - bénévols, salariés")
+    name = models.CharField('Nom du projet',
+                            max_length=100)
+    owner_image = models.ImageField('Photo du chef du projet',
+                                    upload_to=user_directory_path,
+                                    blank=True,
+                                    null=True)
+    creation_date = models.PositiveIntegerField('Date de création',
+                                                blank=True,
+                                                null=True)
+    contact_name = models.CharField('Nom du contact de référence',
+                                    max_length=100,
+                                    blank=True,
+                                    null=True)
+    email = models.EmailField('Email de contact',
+                              blank=True,
+                              null=True)
+    telephone = models.CharField('Numéro de téléphone',
+                                 max_length=14,
+                                 blank=True,
+                                 null=True)
+    web_site = models.URLField('Site web du projet',
+                               blank=True,
+                               null=True)
+    structure = models.CharField('Structure administrative du projet',
+                                 max_length=100,
+                                 blank=True,
+                                 null=True)
+    location_today = models.ImageField("Présence du projet aujourd'hui",
+                                       upload_to=user_directory_path,
+                                       blank=True,
+                                       null=True,)
+    location_target = models.ImageField("Prévision d'évolution géographique",
+                                        upload_to=user_directory_path,
+                                        blank=True,
+                                        null=True)
+    logo = models.ImageField('Logo du projet',
+                             upload_to=user_directory_path,
+                             blank=True,
+                             null=True)
+    project_structure = models.ImageField('Image de la structure globale du projet',
+                                          upload_to=user_directory_path,
+                                          blank=True,
+                                          null=True)
+    workers = models.TextField('Participants (bénévoles, salariés)',
+                               blank=True,
+                               null=True)
 
     def __unicode__(self):
         return unicode(self.name)
@@ -55,20 +78,30 @@ class Person(models.Model):
         return unicode(self.name) + ' ' + unicode(self.surname)
 
 class Role(models.Model):
-    name = models.CharField(max_length=128, default="membre", unique=True,
-                             help_text="Le role d'une personne pour un projet en particulier - membre par défaut")
+
+    class Meta:
+        verbose_name = 'Role'
+
+
+    name = models.CharField('Nom',
+                            max_length=128,
+                            default='membre',
+                            unique=True,
+                            help_text="Le role d'une personne pour un projet en particulier - membre par défaut")
     def __unicode__(self):
         return unicode(self.name)
 
 class Membership(models.Model):
-    person = models.ForeignKey(Person)
-    project = models.ForeignKey(Project)
-    role = models.ForeignKey(Role)
+
+    person = models.ForeignKey(Person, verbose_name='Personne')
+    project = models.ForeignKey(Project, verbose_name='Projet')
+    role = models.ForeignKey(Role, verbose_name='Role')
     def __unicode__(self):
-        return unicode(self.person) + " - " + unicode(self.role) + " du projet " + unicode(self.project)
+        return unicode(self.person) + ' - ' + unicode(self.role) + ' du projet ' + unicode(self.project)
 
 class Story(models.Model):
-    description = models.CharField(null=True,max_length=200)
+    description = models.CharField(null=True,
+                                   max_length=200)
     project = models.ForeignKey(Project)
     content = models.TextField()
     coordinate_x = models.IntegerField()
@@ -80,28 +113,44 @@ class Story(models.Model):
 class Address(models.Model):
 
     class Meta:
-        verbose_name = 'Addresse'
+        verbose_name = 'Adresse'
 
-    address = models.CharField('Adresse', max_length=500, null=True, blank=True)
-    code = models.CharField(max_length=5, help_text="code postal")
-    city = models.CharField('Ville', max_length=100, null=True, blank=True, help_text="ville")
-    country = models.CharField('Pays', max_length=100, default="France", help_text="pays")
-    latitude = models.DecimalField(decimal_places=15, max_digits=18,
-                                   blank=True, null=True)
-    longitude = models.DecimalField(decimal_places=15, max_digits=18,
-                                    blank=True, null=True)
+    address = models.CharField('Adresse',
+                               max_length=500,
+                               null=True,
+                               blank=True)
+    code = models.CharField('Code postal', max_length=5)
+    city = models.CharField('Ville',
+                            max_length=100,
+                            null=True,
+                            blank=True)
+    country = models.CharField('Pays',
+                               max_length=100,
+                               default='France')
+    latitude = models.DecimalField(decimal_places=15,
+                                   max_digits=18,
+                                   blank=True,
+                                   null=True)
+    longitude = models.DecimalField(decimal_places=15,
+                                    max_digits=18,
+                                    blank=True,
+                                    null=True)
 
     def __unicode__(self):
         return self.address + ' ' + self.code + ' ' + self.city
 
 class Event(models.Model):
 
+    class Meta:
+        verbose_name = 'Evénement'
+
     name = models.CharField(max_length=500)
-    start_date = models.DateTimeField(help_text="date de début")
-    end_date = models.DateTimeField(help_text="date de fin")
-    description = models.TextField(null=True, max_length=1000)
-    project = models.ForeignKey(Project, null=True)
-    addresses = models.ManyToManyField(Address)
+    start_date = models.DateTimeField('Date de début')
+    end_date = models.DateTimeField('Date de fin')
+    description = models.TextField(null=True,
+                                   max_length=1000)
+    project = models.ForeignKey(Project, verbose_name='Projet', null=True)
+    addresses = models.ManyToManyField(Address, verbose_name='Adresse')
 
     def __unicode__(self):
         return self.name
